9786e35243b1008e5eadac35753de836261c4bee
itolog <paukanlove@gmail.com>
2020-08-02 17:38:21 +0300

    add port

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 import * as compression from 'compression';
 import * as helmet from 'helmet';
 
+const port = Number(process.env.PORT) || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(helmet());
   app.use(compression());
 
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
